d6b8096bf92146cea5dd675bc8d4b5b43320560c
laia.tortajada <laia.tortajada@auleseebe.upc.edu>
2024-10-31 11:35:45 +0100

    ---

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,13 @@
+#P(x>=13000)
+lambda<- 1/10000
+1-dexp(13000,lambda)
 
+qexp(1-0.2,lambda) #el que fa que superi el 0,2
+
+
+#semilla= 85
+set.seed(85) #és la semilla, cal correr l'exercici sempre des d'aquí
+experiment<-rexp(100,lambda)
+mean(experiment)
+median(experiment)
+var(experiment)
